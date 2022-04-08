38c0ea1f2dd5a7ea63910e3863f782a1a928f4a3
Pravat Surya (psurya) <pravat.surya@quest.com>
2022-04-08 17:28:36 +0530

    first commit

---
+++ src/main/java/com/asokit/user/test.java
@@ -0,0 +1,12 @@
+package com.asokit.user;
+
+import com.asokIT.Test.passWord;
+
+public class test {
+	
+	public static void main(String[] args) {
+		String s1=passWord.encodePassword("PUJA");
+		System.out.println("value="+s1);
+	}
+
+}
