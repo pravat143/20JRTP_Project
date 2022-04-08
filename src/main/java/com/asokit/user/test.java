abb2a25ee38de29b0a95b8460df33638d6127582
Pravat Surya (psurya) <pravat.surya@quest.com>
2022-04-08 21:21:28 +0530

    var baba Add

--- src/main/java/com/asokit/user/test.java
+++ src/main/java/com/asokit/user/test.java
@@ -5,6 +5,7 @@ import com.asokIT.Test.passWord;
 public class test {
 	
 	public static void main(String[] args) {
+		int baba=10;
 		String s1=passWord.encodePassword("PUJA");
 		System.out.println("value="+s1);
 	}
