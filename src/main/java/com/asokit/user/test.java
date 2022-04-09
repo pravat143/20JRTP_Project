b6ad1d6749e657d2931cee7cfd2a8b02ab47e591
Pravat Surya (psurya) <pravat.surya@quest.com>
2022-04-09 23:02:57 +0530

    Puja var add

--- src/main/java/com/asokit/user/test.java
+++ src/main/java/com/asokit/user/test.java
@@ -6,6 +6,8 @@ public class test {
 	
 	public static void main(String[] args) {
 		int baba=10;
+		
+		int puja=20;
 		String s1=passWord.encodePassword("PUJA");
 		System.out.println("value="+s1);
 	}
